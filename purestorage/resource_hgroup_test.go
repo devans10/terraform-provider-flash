d102f32765da472a14daa7f7a3ab5a134fab7e96
Dave Evans <devans@duqlight.com>
2018-12-09 17:22:58 -0500

    added volume and hgroup tests

---
+++ purestorage/resource_hgroup_test.go
@@ -0,0 +1,96 @@
+package purestorage
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/devans10/go-purestorage/flasharray"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+const testAccCheckPureHostgroupResourceName = "purestorage_hostgroup.tfhostgrouptest"
+
+const testAccCheckPureHostgroupConfig = `
+resource "purestorage_hostgroup" "tfhostgrouptest" {
+	name = "tfhostgrouptest"
+}
+`
+
+const testAccCheckPureHostgroupConfigWithHostlist = `
+resource "purestorage_host" "tfhosttest" {
+	name = "tfhosttest"
+}
+
+resource "purestorage_hostgroup" "tfhostgrouptest" {
+        name = "tfhostgrouptest"
+	hostlist = ["${purestorage_host.tfhostest.name}"]
+}
+`
+
+// Create a hostgroup
+func TestAccResourcePureHostgroup_createHostgroup(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPureHostgroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPureHostgroupConfig,
+				Check:  resource.ComposeTestCheckFunc(testAccCheckPureHostgroupExists(testAccCheckPureHostgroupResourceName, true)),
+			},
+		},
+	})
+}
+
+func testAccCheckPureHostgroupDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*flasharray.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "purestorage_hostgroup" {
+			continue
+		}
+
+		_, err := client.Hostgroups.GetHostgroup(rs.Primary.ID, nil)
+		if err != nil {
+			return nil
+		} else {
+			return fmt.Errorf("hostgroup '%s' stil exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckPureHostgroupExists(n string, exists bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		client := testAccProvider.Meta().(*flasharray.Client)
+		_, err := client.Hostgroups.GetHostgroup(rs.Primary.ID, nil)
+		if err != nil {
+			if exists {
+				return fmt.Errorf("hostgroup does not exist: %s", n)
+			}
+			return nil
+		}
+		return nil
+	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
---
+++ purestorage/resource_volume_test.go
@@ -0,0 +1,81 @@
+package purestorage
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/devans10/go-purestorage/flasharray"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+const testAccCheckPureVolumeResourceName = "purestorage_volume.tfvolumetest"
+
+const testAccCheckPureVolumeConfig = `
+resource "purestorage_volume" "tfvolumetest" {
+	name = "tfvolumetest"
+	size = "1G"
+}
+`
+
+// The volumes created in theses tests will not be eradicated.
+// So to run these tests multiple times within 24 hours, the
+// tests volumes will have to be eradicated manually.
+//
+// Create a volume
+func TestAccResourcePureVolume_createVolume(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPureVolumeDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPureVolumeConfig,
+				Check:  resource.ComposeTestCheckFunc(testAccCheckPureVolumeExists(testAccCheckPureVolumeResourceName, true)),
+			},
+		},
+	})
+}
+
+func testAccCheckPureVolumeDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*flasharray.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "purestorage_volume" {
+			continue
+		}
+
+		_, err := client.Volumes.GetVolume(rs.Primary.ID, nil)
+		if err != nil {
+			return nil
+		} else {
+			return fmt.Errorf("volume '%s' stil exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckPureVolumeExists(n string, exists bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		client := testAccProvider.Meta().(*flasharray.Client)
+		_, err := client.Volumes.GetVolume(rs.Primary.ID, nil)
+		if err != nil {
+			if exists {
+				return fmt.Errorf("volume does not exist: %s", n)
+			}
+			return nil
+		}
+		return nil
+	}
+}
